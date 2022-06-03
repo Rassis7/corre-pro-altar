cd4947e31697b848a442d632540cf3a5c7dc0702
Romulo Assis <romulo.assis@ifood.com.br>
2022-06-03 18:25:19 -0300

    refactor: reexport flex

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -3,6 +3,6 @@ export * from "./text";
 export * from "./input";
 export * from "./lottieAnimation";
 export * from "./modal";
-export * from "./flex";
 export * from "./error";
 export * from "./icons";
+export * from "./flex";
