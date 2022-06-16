c932c0f7f9800b747c72f8dae62d6ec1d133c421
Romulo Assis <romulo.assis@ifood.com.br>
2022-06-16 20:07:54 -0300

    feat: add new image provider

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ module.exports = {
   trailingSlash: true,
   reactStrictMode: true,
   images: {
-    domains: ["image.freepik.com", "img.freepik.com", "images.unsplash.com"],
+    domains: [
+      "image.freepik.com",
+      "img.freepik.com",
+      "images.unsplash.com",
+      "images.pexels.com",
+    ],
   },
   env: {
     FAUNADB_KEY: process.env.FAUNADB_KEY,
